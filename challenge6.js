e96937e539c4ef60e32a395ab0042b05f70f9d3d
Laura Guerra <l.garciaguerra1@gmail.com>
2023-02-15 06:11:46 -0500

    Refactor challenge six

--- challenge6.js
+++ challenge6.js
@@ -30,3 +30,40 @@ var upperCase = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 // console.log(upperCase); --> [ 'Turing', 'Is', 'The', 'Best' ]
 var newSentence = upperCase.join(" ");
 console.log(newSentence); // --> Turing Is The Best
+
+/*
+Refactor: 
+1. Define a variable that contains a string.
+2. Access each word in the string by breaking the string into an array: 
+        Call a method (.split(" ")) to break up the string at every instance of a space, 
+        and store the value in a variable that holds an array. 
+    (STEPS 1 & 2 DO NOT NEED TO BE REDONE. THEY CAN BE ACCESSED FROM LINE 25 & 26.)
+3. Declare a function. Within the function: 
+    - Declare a variable that stores an empty string to later concatenate the new sentence. 
+    - Iterate over the array using a for loop. Within the loop: 
+        - Declare a variable that stores the changes to be made to each element in the array. 
+        - Access each element using the index position given by that instance of the loop [i]. 
+        - Make the following changes to each element: 
+            - Call a method (.charAT(0)) to access the character at index position 0 of each 
+              element (first letter of each word). 
+            - Capitalize the identified character by adjoining the .toUpperCase method. 
+            - Add the remainder of each word by calling a function (.slice(1)) to add the 
+              remainder of each element in the array beginning with the character at index 
+              position 1 for that specific element (the second letter from each word). 
+        - Concatonate the varable that stores an empty string with the variable that stores 
+          the changes made to each element and a space " ". 
+    - Outside the loop & within the function, return the variable that stores the new sentence.
+4. Invoke the function and log it to the console.  
+*/
+
+
+function capWords() {
+  var newSentence = ``
+  for (var i = 0; i < words.length; i++) {
+      var newWords = words[i].charAt(0).toUpperCase()+ words[i].slice(1);
+      newSentence += newWords + " ";
+  }
+  return newSentence;
+}
+
+console.log(capWords()); // --> Turing Is The Best
